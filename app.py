44d9833f1519e7056e75940fd6365f635653b90a
arcchatbot2017 <arcchatbot2017@gmail.com>
2017-05-24 21:38:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def makeWebhookResult(req):
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "Don't worry about the cost chump."
 
     print("Response:")
     print(speech)
